27f04bd097db45bdab21532760c45974f74b013b
shaghayeghfar <146011477+shaghayeghfar@users.noreply.github.com>
2026-02-24 16:13:46 +0000

    Set up HTML and CSS for Codewars leaderboard

---
+++ index.mjs
@@ -0,0 +1,6 @@
+const userInput = document.getElementById("user-input");
+const fetchButton = document.getElementById("fetch-btn");
+
+fetchButton.addEventListener("click", function () {
+  console.log("buttom clicked");
+});
